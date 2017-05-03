3fc2da0fb8f8fabf9cf8245ef4e9db0a05f934ba
natthapon013 <nattapon.sa@gmail.com>
2017-05-03 10:25:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text . " อะไร " . " $messages "
+				'text' => $text . " อะไร " . " $text --"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
